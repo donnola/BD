aa9dcb5bdba6aacdb3a8b0858058a601e1c4f99d
donnola <47623998+donnola@users.noreply.github.com>
2019-04-30 15:37:08 +0300

    Update CRUD.sql

--- CRUD.sql
+++ CRUD.sql
@@ -11,6 +11,9 @@ from product
 where product_id not in (Select distinct product_id
                          from products_in_order
                          group by products_in_order.product_id);
+                         
+insert into product
+values (865734, 'огнетушитель', 4000);
 
 insert into employee
 values (432523, 'Арина', 'Петрова', 'бухгалтер', 1, 'Y', 70000, '+7(977)246-75-32', 'Petro@mail.ru');
