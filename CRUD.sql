ceb5721ee20430789cfc7ee4734c3230b7069278
donnola <47623998+donnola@users.noreply.github.com>
2019-05-02 12:47:01 +0300

    Update CRUD.sql

--- CRUD.sql
+++ CRUD.sql
@@ -1,19 +1,20 @@
 update "order"
-set status = 'в пути'
+set status_desc = 'в пути'
 where order_id = 018493;
 
 select *
 from "order"
-where status = 'в пути';
+where status_desc = 'в пути';
 
 delete
 from product
 where product_id not in (Select distinct product_id
                          from products_in_order
                          group by products_in_order.product_id);
-                         
+
 insert into product
 values (865734, 'огнетушитель', 4000);
+
 
 insert into employee
 values (432523, 'Арина', 'Петрова', 'бухгалтер', 1, 'Y', 70000, '+7(977)246-75-32', 'Petro@mail.ru');
