008b0d4eca89a6f869ca7df03a63cb9f327b763f
donnola <47623998+donnola@users.noreply.github.com>
2019-05-12 19:23:39 +0300

    Update DDL_code_create.sql

--- DDL_code_create.sql
+++ DDL_code_create.sql
@@ -31,9 +31,9 @@ create table storages.order (
   status_desc varchar(50) NOT NULL ,
   storage_id int,
   pickup_point_id int,
-  FOREIGN KEY (client_id) REFERENCES client_with_order (client_id),
-  FOREIGN KEY (storage_id) REFERENCES storage (storage_id),
-  FOREIGN KEY (pickup_point_id) REFERENCES pickup_point (pickup_point_id)
+  FOREIGN KEY (client_id) REFERENCES storages.client_with_order (client_id),
+  FOREIGN KEY (storage_id) REFERENCES storages.storage (storage_id),
+  FOREIGN KEY (pickup_point_id) REFERENCES storages.pickup_point (pickup_point_id)
 );
 
 create table storages.product (
@@ -52,31 +52,31 @@ create table storages.employee (
   salary_amt real NOT NULL CHECK (salary_amt > 0),
   telephone_number_no varchar(50) NOT NULL UNIQUE ,
   email_txt varchar(50) NOT NULL UNIQUE,
-  FOREIGN KEY (storage_id) REFERENCES storage (storage_id)
+  FOREIGN KEY (storage_id) REFERENCES storages.storage (storage_id)
 );
 
 create table storages.packer_of_position_in_order (
   order_id int,
   product_id int NOT NULL ,
   employee_id int,
-  FOREIGN KEY (order_id) REFERENCES "order" (order_id),
-  FOREIGN KEY (employee_id) REFERENCES employee (employee_id)
+  FOREIGN KEY (order_id) REFERENCES storages.order (order_id),
+  FOREIGN KEY (employee_id) REFERENCES storages.employee (employee_id)
 );
 
 create table storages.products_in_order (
   order_id int,
   product_id int,
   quantity_of_product_cnt int NOT NULL CHECK (quantity_of_product_cnt > 0),
-  FOREIGN KEY (order_id) REFERENCES "order" (order_id),
-  FOREIGN KEY (product_id) REFERENCES product (product_id)
+  FOREIGN KEY (order_id) REFERENCES storages.order (order_id),
+  FOREIGN KEY (product_id) REFERENCES storages.product (product_id)
 );
 
 create table storages.products_on_storage (
   storage_id int,
   product_id int,
   quantity_of_product_cnt int,
-  FOREIGN KEY (storage_id) REFERENCES storage (storage_id),
-  FOREIGN KEY (product_id) REFERENCES product (product_id)
+  FOREIGN KEY (storage_id) REFERENCES storages.storage (storage_id),
+  FOREIGN KEY (product_id) REFERENCES storages.product (product_id)
 );
 
 SET
